41aa3e123cc56f4894b692b9e4d1248f450e5376
Francesco Napolitano <franconapa96@gmail.com>
2019-02-25 20:51:18 +0100

    Custom Layout Image View. Image view with Timer. active_dot and nonactive_dot viewPageAdapter for custom view

--- app/src/main/java/barletta/coding/barlettapp/HomeActivity.java
+++ app/src/main/java/barletta/coding/barlettapp/HomeActivity.java
@@ -1,22 +1,80 @@
 package barletta.coding.barlettapp;
 
 import android.content.Intent;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class HomeActivity extends AppCompatActivity {
     //TUTTO DI PROVA. SERVE SOLO A CAPIRE COME PRENDERE I DATI
-    private TextView usernameShow, emailShow, idshow;
+
     private Button logoutB;
     private int tipo;
-
+    ViewPager viewPager;
+    LinearLayout sliderDotspanel;
+    private int dotscount;
+    private ImageView[] dots;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        viewPager = findViewById(R.id.slideShowHome);
+
+        viewPageAdapter viewPageAdapt = new viewPageAdapter(this);
+
+        viewPager.setAdapter(viewPageAdapt);
+
+        sliderDotspanel = (LinearLayout) findViewById(R.id.SliderDots);
+
+        dotscount = viewPageAdapt.getCount();
+
+        dots = new ImageView[dotscount];
+
+        for(int i = 0; i<dotscount;i++){
+
+            dots[i] = new ImageView(this);
+            dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.nonactive_dot));
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.setMargins(8, 0,8,0);
+
+            sliderDotspanel.addView(dots[i], params);
+
+        }
+
+        dots[0].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+                for(int i = 0; i< dotscount; i++){
+                    dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.nonactive_dot));
+                }
+
+                dots[position].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new MyTimerTask(), 2000, 4000);
 
         //Come prima cosa, vediamo se l'utente è loggato, se no, lo mandiamo al login
         if(!SharedPrefManager.getInstance(this).isLogged()){
@@ -24,19 +82,10 @@ public class HomeActivity extends AppCompatActivity {
             startActivity(new Intent(this,LoginAndRegister.class));
         }
 
-        usernameShow = findViewById(R.id.textViewUsername);
-        emailShow = findViewById(R.id.textViewEmail);
-        idshow = findViewById(R.id.textViewId);
         logoutB = findViewById(R.id.buttonLogOut);
-        usernameShow.setText(SharedPrefManager.getInstance(this).getUsername());
-        emailShow.setText(SharedPrefManager.getInstance(this).getUserEmail());
+
         tipo = SharedPrefManager.getInstance(this).getTipo();
-        if(tipo == 1){
-            idshow.setText("tipo 1");
-        }
-        if(tipo == 2){
-            idshow.setText("tipo 2");
-        }
+
 
         logoutB.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,6 +99,30 @@ public class HomeActivity extends AppCompatActivity {
 
     }
 
+
+    public class MyTimerTask extends TimerTask{
+
+        @Override
+        public void run() {
+
+            HomeActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+
+                    if(viewPager.getCurrentItem() == 0){
+                        viewPager.setCurrentItem(1);
+
+                    }else if(viewPager.getCurrentItem() == 1){
+                        viewPager.setCurrentItem(2);
+                    }else {
+                        viewPager.setCurrentItem(0);
+                    }
+
+                }
+            });
+
+        }
+    }
 
 
 }
---
+++ app/src/main/java/barletta/coding/barlettapp/viewPageAdapter.java
@@ -0,0 +1,68 @@
+package barletta.coding.barlettapp;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class viewPageAdapter extends PagerAdapter {
+
+    private Context context;
+    private LayoutInflater layoutInflater;
+    private Integer[] images = {R.drawable.slide1, R.drawable.slide2, R.drawable.slide3};
+    public  viewPageAdapter(Context context){
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+        return view == o;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, final int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.custom_layout, null);
+        ImageView imageView = view.findViewById(R.id.imageViewCustom);
+        imageView.setImageResource(images[position]);
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(position == 0){
+                    //FAI ROBE
+                    Toast.makeText(context, "Loli 1", Toast.LENGTH_SHORT).show();
+                }
+                else if(position == 1){
+                    Toast.makeText(context, "Loli 2", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(context, "Loli 3", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        ViewPager vp = (ViewPager) container;
+        vp.addView( view, 0);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+
+        ViewPager vp = (ViewPager) container;
+        View view = (View) object;
+        vp.removeView(view);
+    }
+}
